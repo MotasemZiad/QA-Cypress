8ee9252bcab530c075bb98307fc777fa762c1481
Motasem Abunima <motasemabunima@gmail.com>
2023-08-29 14:57:28 +0300

    refactoring...

--- cypress/e2e/personal_financing_page/personal-financing_spec.cy.js
+++ cypress/e2e/personal_financing_page/personal-financing_spec.cy.js
@@ -1,42 +1,10 @@
 describe("Personal Financing Calculator Test Suite", () => {
   it("User should be able to calculate personal financing with valid data", () => {
-    cy.visit("https://waset.sa/personal-financing");
-    cy.get("#type").select("New", { force: true });
-    cy.get("#sector_code").select("Government", { force: true });
-    cy.get("#entity_id").select("SANABIL INVESTMENT", { force: true });
-    cy.get("#amount_financed").type("1");
-    cy.get("#salary").type("1");
-    cy.get(".credit-obligations-value").type("3200");
-    cy.get(".living-expenses-value").type("2300");
-    cy.get("#service_months_no").select("One Month", { force: true });
-    cy.get("#bank_code").select("Samba", { force: true });
-    cy.get("#mortgage_loan").select("No", { force: true });
-    cy.get("#trouble").select("No", { force: true });
-    cy.get("#nationality").select("Saudi", { force: true });
-    cy.get("#day").select("1", { force: true });
-    cy.get("#month").select("Muharram", { force: true });
-    cy.get("#year2").select("1424", { force: true });
-    cy.get("button[type='submit']").click();
+    enterPersonalFinancingInformation();
   });
 
   it("User should be able to enter his data", () => {
-    cy.visit("https://waset.sa/personal-financing");
-    cy.get("#type").select("New", { force: true });
-    cy.get("#sector_code").select("Government", { force: true });
-    cy.get("#entity_id").select("SANABIL INVESTMENT", { force: true });
-    cy.get("#amount_financed").type("1");
-    cy.get("#salary").type("1");
-    cy.get(".credit-obligations-value").type("3200");
-    cy.get(".living-expenses-value").type("2300");
-    cy.get("#service_months_no").select("One Month", { force: true });
-    cy.get("#bank_code").select("Samba", { force: true });
-    cy.get("#mortgage_loan").select("No", { force: true });
-    cy.get("#trouble").select("No", { force: true });
-    cy.get("#nationality").select("Saudi", { force: true });
-    cy.get("#day").select("1", { force: true });
-    cy.get("#month").select("Muharram", { force: true });
-    cy.get("#year2").select("1424", { force: true });
-    cy.get("button[type='submit']").click();
+    enterPersonalFinancingInformation();
 
     // Go to the next step
     cy.get("input[name='name']").type("hany srour");
@@ -59,3 +27,23 @@ describe("Personal Financing Calculator Test Suite", () => {
     cy.get("button[type='submit']").click();
   });
 });
+
+function enterPersonalFinancingInformation() {
+  cy.visit("https://waset.sa/personal-financing");
+  cy.get("#type").select("New", { force: true });
+  cy.get("#sector_code").select("Government", { force: true });
+  cy.get("#entity_id").select("SANABIL INVESTMENT", { force: true });
+  cy.get("#amount_financed").type("1");
+  cy.get("#salary").type("1");
+  cy.get(".credit-obligations-value").type("3200");
+  cy.get(".living-expenses-value").type("2300");
+  cy.get("#service_months_no").select("One Month", { force: true });
+  cy.get("#bank_code").select("Samba", { force: true });
+  cy.get("#mortgage_loan").select("No", { force: true });
+  cy.get("#trouble").select("No", { force: true });
+  cy.get("#nationality").select("Saudi", { force: true });
+  cy.get("#day").select("1", { force: true });
+  cy.get("#month").select("Muharram", { force: true });
+  cy.get("#year2").select("1424", { force: true });
+  cy.get("button[type='submit']").click();
+}
