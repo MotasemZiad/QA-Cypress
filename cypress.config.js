cee9766694df6a380ae7277c5000459026ab2a2b
Motasem Abunima <motasemabunima@gmail.com>
2023-08-30 16:44:43 +0300

    some updates

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'vjbkzj',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/samirmansour.ps/home_page_spec.cy.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-
-describe("Home Page Test Suite", () => {
-  beforeEach(() => {
-    cy.visit("https://samirmansour.ps/");
-  });
-});
